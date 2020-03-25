a6ea5169483084f45aee81beb15ccc8f83620660
pushkarpm24 <pushkarmorey555@gmail.com>
2020-03-25 10:47:07 +0530

    ADDED: code to setting the board

---
+++ ticTacToe.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+# Problem Statement -> Setting the board for Tic Tac Toe
+# Author -> Pushkar Morey
+# Date -> 23/03/2020
+
+
+for (( i=0; i<3; i++ ))
+do
+			echo "---------------"
+		for (( j=0; j<3; j++ ))
+		do
+			printf "|   |"
+		done
+			printf "\n"
+
+done
+			echo "---------------"
